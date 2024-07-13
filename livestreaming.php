8154e62199377ac8a3794199bbce2f5ca9cbfe35
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-07-14 01:25:51 +0630

    feat: add live streaming safety

---
+++ livestreaming.php
@@ -0,0 +1,70 @@
+<?php include('header.php'); ?>
+
+<header class="site-header d-flex flex-column justify-content-center align-items-center">
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+
+            <div class="col-lg-5 col-12 mb-5">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Most Popular Social Media Apps</a></li>
+
+                        <li class="breadcrumb-item active" aria-current="page">livestreaming</li>
+                    </ol>
+                </nav>
+
+                <h2 class="text-white">How to Stay Safe on livestreaming</h2>
+
+                <div class="d-flex align-items-center mt-5">
+                    <a href="#topics-detail" class="btn custom-btn custom-border-btn smoothscroll me-4">Read More</a>
+
+                    <a href="#top" class="custom-icon bi-bookmark smoothscroll"></a>
+                </div>
+            </div>
+
+            <div class="col-lg-5 col-12">
+                <div class="topics-detail-block bg-white shadow-lg">
+                    <img src="images/topics/undraw_Podcast_audience_re_4i5q.png" class="custom-block-image img-fluid" alt="">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</header>
+
+
+<section class="topics-detail-section section-padding" id="topics-detail">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-8 col-12 m-auto">
+                <h3 class="mb-4">How to Stay Safe on livestreaming</h3>
+
+                <p>With all social media, there comes risk. Risk associated with privacy, data and vulnerabilities. Where videos could once be censored and taken down, when acts unfold live over the internet, it is often a lot less containable. Most recently, there have been some horrific and disturbing results, with murder, rape and other horrific violent crimes occurring in real-time on live streaming channels.
+                </p>
+
+                <p>It is not only the threat of young people being exposed to uncensored and horrific content that live streaming poses, but there are risks as well to those who are shown on live streaming videos that need to be considered. For instance:</p>
+
+                <blockquote>
+                    the danger of self broadcasting
+                </blockquote>
+
+                <p>
+                    <strong>Issue of Consent: </strong>When live streaming, it is broadcast instantly for all to see, so can quickly involve those who do not want to be included or have not given their consent.
+                </p>
+                <p>
+                    <strong>Accidentally releasing their location: </strong>For instance, if a teen live streams on their street, everyone will know where they live.
+                </p>
+                <p>
+                    <strong>Revealing intimate information: </strong>Live streaming could lead to young people revealing too much personal information about themselves. Even publishing live video in their own bedrooms could give away clues to their identity or other personal information.
+                </p>
+                <p>
+                    <strong>Not knowing who is watching: </strong>Live streaming services have limited privacy controls, and so it is hard to know who is watching and prevent people accessing the stream.
+                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
