6a3b59d7908813474e145ae92248b280a259c2e8
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-07-13 23:14:57 +0630

    feat: add about us page

---
+++ about-us.php
@@ -0,0 +1,52 @@
+<?php include('header.php'); ?>
+
+<section class="timeline-section section-padding">
+    <div class="section-overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 col-12 mx-auto">
+                <div class="col-12 text-center">
+                    <h2 class="mb-4 text-white">About Us</h2>
+                </div>
+                <p class="text-white">
+                    Our company is dedicated to empowering teenagers with the information and tools to navigate social media safely. We provide support and education, helping teenagers understand the risks of social media apps. Through our online platform, we offer relevant resources and suggestions on digital safety. Additionally, our website offers a membership section where customers may join up for a monthly email, ensuring they stay current with the newest insights and recommendations.
+                </p>
+                <div class="timeline-container">
+                    <ul class="vertical-scrollable-timeline" id="vertical-scrollable-timeline">
+                        <div class="list-progress">
+                            <div class="inner"></div>
+                        </div>
+                        <li>
+                            <h4 class="text-white mb-3">Mission</h4>
+                            <p class="text-white">To educate and empower youth with the information and tools to utilize social media securely, fostering a responsible and positive digital experience.</p>
+                            <div class="icon-holder">
+                                <i class="bi-graph-up"></i>
+                            </div>
+                        </li>
+                        <li>
+                            <h4 class="text-white mb-3">Vision</h4>
+                            <p class="text-white">Our vision is a digital landscape where youth interact with social media confidently and responsibly, supported by a community dedicated to promoting online safety and well-being.
+                            </p>
+                            <div class="icon-holder">
+                                <i class="bi-gem"></i>
+                            </div>
+                        </li>
+                        <li>
+                            <h4 class="text-white mb-3">Value</h4>
+                            <p class="text-white">We place teenagers' safety and welfare first in all of our initiatives, encouraging responsible online conduct and safeguarding their privacy.
+                            </p>
+                            <p class="text-white">We think it's crucial to teach youngsters the knowledge and talents they need to make sensible decisions regarding their internet presence.
+                            </p>
+                            <p class="text-white">We embrace ethical principles in all areas of our campaign, pushing for norms and practices that promote openness, justice, and respect online.</p>
+                            <div class="icon-holder">
+                                <i class="bi-book"></i>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
