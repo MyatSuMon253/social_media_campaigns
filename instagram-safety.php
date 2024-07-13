b000128d1be5b27cd0782a14255ed9f3442d676f
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-07-14 01:25:24 +0630

    feat: add instagram safety

---
+++ instagram-safety.php
@@ -0,0 +1,77 @@
+<?php include('header.php'); ?>
+
+<header class="site-header d-flex flex-column justify-content-center align-items-center">
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+
+            <div class="col-lg-5 col-12 mb-5">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Most Popular Social Media Apps</a></li>
+
+                        <li class="breadcrumb-item active" aria-current="page">instagram</li>
+                    </ol>
+                </nav>
+
+                <h2 class="text-white">How to Stay Safe on Instagram</h2>
+
+                <div class="d-flex align-items-center mt-5">
+                    <a href="#topics-detail" class="btn custom-btn custom-border-btn smoothscroll me-4">Read More</a>
+
+                    <a href="#top" class="custom-icon bi-bookmark smoothscroll"></a>
+                </div>
+            </div>
+
+            <div class="col-lg-5 col-12">
+                <div class="topics-detail-block bg-white shadow-lg">
+                    <img src="images/topics/undraw_online_ad_re_ol62.png" class="custom-block-image img-fluid" alt="">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</header>
+
+
+<section class="topics-detail-section section-padding" id="topics-detail">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-8 col-12 m-auto">
+                <h3 class="mb-4">How to Stay Safe on Instagram</h3>
+
+                <p>Instagram is the most popular social media for sharing photographs and videos. It is a terrific technique to communicate what you're up to, follow mates and stars, and locate images and videos about activities you're interested in.
+                </p>
+
+                <p> Instagram could appear a lot of fun, but making sure you know about the numerous safety and privacy choices guarantees you will only connect with those you want to share your activities.</p>
+
+                <blockquote>
+                    Never share your password with anyone, even if they claim to be from Instagram.
+                </blockquote>
+
+                <p>
+                    <strong>Account Privacy: </strong>Set your account to private to limit who sees your posts. Remove undesired followers without alerting them.
+                </p>
+                <p>
+                    <strong>Content Control: </strong>Delete or archive articles, hide stories from specific individuals, and create Close Friends lists for selective sharing.
+                </p>
+                <p>
+                    <strong>Comment Management: </strong>Limit who can comment, block certain commentators, and use filters to hide inappropriate remarks.
+                </p>
+                <p>
+                    <strong>Wellbeing: </strong>Monitor your activity, mute notifications, and be cautious with location sharing.
+                </p>
+                <p>
+                    <strong>Use 2FA Method: </strong>Add additional layer of protection to show that you are the genuine owner.
+                </p>
+                <p>
+                    <strong>Do not click on suspicious links: </strong>Be careful when clicking on links in messages or posts.
+
+                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
