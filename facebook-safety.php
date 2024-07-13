50384f495b51e99c3a569cb435fdd0f66027f10a
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-07-13 23:14:40 +0630

    feat: how to safe on facebook

---
+++ facebook-safety.php
@@ -0,0 +1,80 @@
+<?php include('header.php'); ?>
+
+<header class="site-header d-flex flex-column justify-content-center align-items-center">
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+
+            <div class="col-lg-5 col-12 mb-5">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Most Popular Social Media Apps</a></li>
+
+                        <li class="breadcrumb-item active" aria-current="page">facebook</li>
+                    </ol>
+                </nav>
+
+                <h2 class="text-white">How to Stay Safe on Facebook</h2>
+
+                <div class="d-flex align-items-center mt-5">
+                    <a href="#topics-detail" class="btn custom-btn custom-border-btn smoothscroll me-4">Read More</a>
+
+                    <a href="#top" class="custom-icon bi-bookmark smoothscroll"></a>
+                </div>
+            </div>
+
+            <div class="col-lg-5 col-12">
+                <div class="topics-detail-block bg-white shadow-lg">
+                    <img src="images/topics/undraw_Remote_design_team_re_urdx.png" class="topics-detail-block-image img-fluid">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</header>
+
+
+<section class="topics-detail-section section-padding" id="topics-detail">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-8 col-12 m-auto">
+                <h3 class="mb-4">How to Stay Safe on Facebook</h3>
+
+                <p>Facebook may be an amazing platform to reconnected with our old friends, stay up to date with family and friends, and share our opinion within community. It can be safe when you take use of its Facebook settings and are careful about the information you share.
+                </p>
+
+                <p>In this post we talk about some of the common scams on Facebook, how to avoid them, and best suggestions to help you protect your privacy.</p>
+
+                <blockquote>
+                    Never share your password with anyone, even if they claim to be from Facebook.
+                </blockquote>
+
+                <p>
+                    <strong>Customize your privacy options: </strong>Customize settings that control who may view your posts and comments.
+                </p>
+                <p>
+                    <strong>Do not overshare personal information: </strong>Be careful when you provide personal information to prevent identity theft.
+                </p>
+                <p>
+                    <strong>Give a strong password: </strong>Use a strong password including letters and digits for better security.
+                </p>
+                <p>
+                    <strong>Do not accept friend requests from unknown: </strong>Only accept friend requests from people you really know in outside world.
+                </p>
+                <p>
+                    <strong>Use 2FA Method: </strong>Add additional layer of protection to show that you are the genuine owner.
+                </p>
+                <p>
+                    <strong>Do not use Facebook to log in to other websites: </strong>Using Facebook to sign in to other sites can disclose your personal information with those websites.
+                </p>
+                <p>
+                    <strong>Do not click on suspicious links: </strong>Be careful when clicking on links in messages or posts.
+
+                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
