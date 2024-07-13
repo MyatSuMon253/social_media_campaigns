fa9d9b68216f2e9927355ef55bec9ab518202dc6
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-07-14 01:28:22 +0630

    feat: list of popular apps

---
+++ popular-apps.php
@@ -0,0 +1,99 @@
+<?php include('header.php'); ?>
+
+<section class="section-padding">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-12 col-12 text-center">
+                <h3 class="mb-4">Risks of Social Media Apps</h3>
+            </div>
+
+            <div class="col-lg-8 col-12 mt-3 mx-auto">
+                <div class="custom-block custom-block-topics-listing bg-white shadow-lg mb-5">
+                    <div class="d-flex">
+                        <img src="images/topics/undraw_Remote_design_team_re_urdx.png" class="custom-block-image img-fluid" alt="">
+
+                        <div class="custom-block-topics-listing-info d-flex">
+                            <div>
+                                <h5 class="mb-2">How to Stay Safe on Facebook</h5>
+
+                                <p class="mb-0">Facebook may be an amazing platform to reconnected with our old friends, stay up to date with family and friends, and share our opinion within community. It can be safe when you take use of its Facebook settings and are careful about the information you share.</p>
+
+                                <a href="facebook-safety.php" class="btn custom-btn mt-3 mt-lg-4">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="custom-block custom-block-topics-listing bg-white shadow-lg mb-5">
+                    <div class="d-flex">
+                        <img src="images/topics/undraw_online_ad_re_ol62.png" class="custom-block-image img-fluid" alt="">
+
+                        <div class="custom-block-topics-listing-info d-flex">
+                            <div>
+                                <h5 class="mb-2">How to Stay Safe on Instagram</h5>
+
+                                <p class="mb-0">
+                                    Instagram is the most popular social media for sharing photographs and videos. It is a terrific technique to communicate what you're up to, follow mates and stars, and locate images and videos about activities you're interested in.
+                                </p>
+
+                                <a href="instagram-safety.php" class="btn custom-btn mt-3 mt-lg-4">Learn More</a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="custom-block custom-block-topics-listing bg-white shadow-lg mb-5">
+                    <div class="d-flex">
+                        <img src="images/topics/undraw_Podcast_audience_re_4i5q.png" class="custom-block-image img-fluid" alt="">
+
+                        <div class="custom-block-topics-listing-info d-flex">
+                            <div>
+                                <h5 class="mb-2">How to Stay Safe on livestreaming</h5>
+
+                                <p class="mb-0">With all social media, there comes risk. Risk associated with privacy, data and vulnerabilities. Where videos could once be censored and taken down, when acts unfold live over the internet, it is often a lot less containable. Most recently, there have been some horrific and disturbing results, with murder, rape and other horrific violent crimes occurring in real-time on live streaming channels.</p>
+
+                                <a href="livestreaming.php" class="btn custom-btn mt-3 mt-lg-4">Learn More</a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12 col-12">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-center mb-0">
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">Prev</span>
+                            </a>
+                        </li>
+
+                        <li class="page-item active" aria-current="page">
+                            <a class="page-link" href="#">1</a>
+                        </li>
+
+                        <li class="page-item">
+                            <a class="page-link" href="#">2</a>
+                        </li>
+
+                        <li class="page-item">
+                            <a class="page-link" href="#">3</a>
+                        </li>
+
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
