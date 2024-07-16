5a37287f71c170a21f0104ec96c854a1cc30eac6
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-07-17 00:17:59 +0630

    feat: link js file in header

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
-<!doctype html>
-<html lang="en">
+
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="social media campaigns">
     <title>SMC Ltd</title>
+
     <!-- CSS FILES -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,57 +13,11 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap-icons.css" rel="stylesheet">
     <link href="css/styles.css" rel="stylesheet">
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/click-scroll.js"></script>
+    <script src="js/custom.js"></script>
 </head>
-
-<body id="top">
-
-    <main>
-
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <a class="navbar-brand" href="index.php">
-                    <span>SMC</span>
-                </a>
-
-                <div class="d-lg-none ms-auto me-4">
-                    <a href="#top" class="navbar-icon bi-person smoothscroll"></a>
-                </div>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-lg-5 me-lg-auto">
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="index.php">Home</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="about-us.php">About Us</a>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">How to Stay Safe</a>
-
-                            <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                                <li><a class="dropdown-item" href="popular-apps.php">Most Popular Social Media Apps</a></li>
-
-                                <li><a class="dropdown-item" href="livestreaming.php">Livestreaming</a></li>
-
-                                <li><a class="dropdown-item" href="how-to-help.php">How Parents Can Help</a></li>
-                            </ul>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="contact-us.php">Contact</a>
-                        </li>
-
-                    </ul>
-
-                    <div class="d-none d-lg-block">
-                        <a href="sign-in.php" class="navbar-icon bi-person smoothscroll"></a>
-                    </div>
-                </div>
-            </div>
-        </nav>
